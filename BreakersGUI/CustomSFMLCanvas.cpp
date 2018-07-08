08aa5d751097f662c32774e05535815ce0336792
michalStankiewicz <michal.stankicz@o2.pl>
2018-07-08 23:48:09 +0200

    Placeholder gui

--- BreakersGUI/CustomSFMLCanvas.cpp
+++ BreakersGUI/CustomSFMLCanvas.cpp
@@ -14,5 +14,23 @@ void CustomSFMLCanvas::OnInit()
 
 void CustomSFMLCanvas::OnUpdate()
 {
+
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+    {
+        shape.move({10* 0.016f,0});
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+    {
+        shape.move({-10* 0.016f,0});
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+    {
+        shape.move({0,-10* 0.016f});
+    }
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    {
+        shape.move({0,10* 0.016f});
+    }
+
     RenderWindow::draw(shape);
 }
